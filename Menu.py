0179755062ace892e46263e3253c5772a3c34af1
Jayden-Briffa <jaydenbriffa@outlook.com>
2026-01-08 10:02:00 +0000

    feature/Implemented basic business logic to main.py

--- Menu.py
+++ Menu.py
@@ -1,5 +1,14 @@
 class Menu:
     MENU_BUFFER = 10
-    def get_user_input(self, menu_choices):
-        print("="*Menu.MENU_BUFFER, "What would you like to do?", "="*MENU_BUFFER)
-        return
+    ERR_BUFFER = 3
+
+    def __init__(self, choices: dict) -> None:
+        self.menu_choices = choices
+        self.choices = choices.keys()
+                
+    def get_user_menu_choice(self) -> str:
+        print("="*Menu.MENU_BUFFER, "What would you like to do?", "="*Menu.MENU_BUFFER)
+        return input()
+    
+    def err(self, msg: str) -> None:
+        return msg
--- main.py
+++ main.py
@@ -1,5 +1,64 @@
 from Menu import Menu
 from Tasks_Model import Tasks_Model
 
+MENU_CHOICES = {
+    "1": "Add a new task",
+    "2": "Retrieve next task",
+    "3": "Retrieve task by ID",
+    "4": "Mark a task as complete"
+}
+
 client = Tasks_Model()
-menu = Menu()
+main_menu = Menu(MENU_CHOICES)
+
+user_choice = None
+while user_choice != "quit":
+    user_choice = main_menu.get_user_menu_choice()
+
+    match user_choice:
+
+        case "1":
+            description = input("Enter a description for the new task: ")
+            priority = input("Enter a priority for the new task: ")
+
+            try:
+                priority = int(priority)
+                new_task = client.add_task(description, priority)
+                print(new_task)
+            except ValueError:
+                main_menu.err("Field 'priority' must be an integer")
+
+        case "2":
+            next_task = client.get_task_by_priority()
+            print(next_task)
+
+        case "3":
+            id = input("Enter the task's id: ")
+
+            try:
+                id = int(id)
+                task = client.add_task(description, id)
+                print(task)
+
+            except ValueError:
+                main_menu.err("Field 'id' must be an integer")
+
+            client.get_task_by_id()
+
+        case "4":
+            id = input("Enter the task's id: ")
+
+            try:
+                id = int(id)
+                task = client.add_task(description, id)
+                print(task)
+
+            except ValueError:
+                main_menu.err("Field 'id' must be an integer")
+
+            client.complete_task()
+
+        case _:
+            main_menu.err(f"Your choice '{user_choice}' is invalid. You can only choose: '{main_menu.choices}'")
+
+exit()
